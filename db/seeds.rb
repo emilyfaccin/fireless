5f4034ca9ced7da20c47ad33956218882ba00237
Emily Faccin <emily.faccin@gmail.com>
2021-11-16 21:06:46 -0300

    add: populates seed file to populate db once created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+8.times { |n| FireReport.create(street: "Rua #{n}", district: "Jardim #{n}", size: "pequeno") }
+10.times { |n| FireReport.create(street: "Alameda #{n}", district: "Vila #{n}", size: "medio") }
+6.times { |n| FireReport.create(street: "Avenida #{n}", district: "Parque #{n}", size: "grande") }
